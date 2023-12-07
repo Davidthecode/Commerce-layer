b6f2f3bc30c69bd352df3bea2a8492590ee90e13
David Ajibola <ajiboladavid0963@gmail.com>
2023-12-07 17:06:57 +0100

    made changes

--- src/components/DevExperience.tsx
+++ src/components/DevExperience.tsx
@@ -1,52 +1,51 @@
 function DevExperience() {
-    return (
-      <div className="bg-black mt-8 pb-10 xl:px-8">
-        <div className="mx-8 alg:mx-16">
-          <h1 className="text-white text-4xl font-medium pt-20 mb-8 opacity-2 alg:text-7xl">
-            It all starts with a great developer experience.
-          </h1>
+  return (
+    <div className="bg-black mt-8 pb-10 xl:px-8">
+      <div className="mx-8 alg:mx-16">
+        <h1 className="text-white text-4xl font-medium pt-20 mb-8 opacity-2 alg:text-7xl">
+          It all starts with a great developer experience.
+        </h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 alg:mt-24 alg:pb-12">
-            <div className="bg-[#272828] text-white p-6 rounded-lg alg:px-10 xl:h-[30rem]">
-              <h1 className="text-xl font-medium alg:text-2xl">Micro frontends</h1>
-              <p className="mt-2 opacity-50 alg:text-lg alg:mt-4">
-                The fastest and simplest way to make any website shoppable. Start
-                with our micro frontends to build a successful proof of concept,
-                even inside your existing monolith. Ship code in minutes, not
-                weeks, and see how a composable, headless approach can work for
-                you.
-              </p>
-              <p className="mt-2 opacity-50 alg:mt-6 alg:text-lg alg:font-medium">Learn more {'->'}</p>
-            </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 alg:mt-24 alg:pb-12">
+          <div className="bg-[#272828] text-white p-6 rounded-lg alg:px-10 xl:h-[30rem]">
+            <h1 className="text-xl font-medium alg:text-2xl">Micro frontends</h1>
+            <p className="mt-2 opacity-50 alg:text-lg alg:mt-4">
+              The fastest and simplest way to make any website shoppable. Start
+              with our micro frontends to build a successful proof of concept,
+              even inside your existing monolith. Ship code in minutes, not
+              weeks, and see how a composable, headless approach can work for
+              you.
+            </p>
+            <p className="mt-2 opacity-50 alg:mt-6 alg:text-lg alg:font-medium">Learn more {'->'}</p>
+          </div>
 
-            <div className="bg-[#272828] text-white p-6 rounded-lg">
-              <h2 className="text-xl font-medium alg:text-2xl">API reference</h2>
-              <p className="mt-2 opacity-50 alg:text-lg alg:mt-4">
-                Explore our developer documentation and learn how to build any
-                business model. With hundreds of API resources and webhooks, you
-                have all the flexibility you need. A recipe-like collection of
-                step-by-step tutorials will guide you through the most common
-                scenarios.
-              </p>
-              <p className="mt-2 opacity-50 alg:mt-12 alg:text-lg alg:font-medium">Learn more {'->'}</p>
-            </div>
+          <div className="bg-[#272828] text-white p-6 rounded-lg">
+            <h2 className="text-xl font-medium alg:text-2xl">API reference</h2>
+            <p className="mt-2 opacity-50 alg:text-lg alg:mt-4">
+              Explore our developer documentation and learn how to build any
+              business model. With hundreds of API resources and webhooks, you
+              have all the flexibility you need. A recipe-like collection of
+              step-by-step tutorials will guide you through the most common
+              scenarios.
+            </p>
+            <p className="mt-2 opacity-50 alg:mt-12 alg:text-lg alg:font-medium">Learn more {'->'}</p>
+          </div>
 
-            <div className="bg-[#272828] text-white p-6 rounded-lg">
-              <h2 className="text-xl font-medium alg:text-2xl">CLI</h2>
-              <p className="mt-2 opacity-50 alg:text-lg alg:mt-4">
-                Use our super-handy Command Line Interface to build a complete
-                ecommerce experience straight from the terminal. Manage all your
-                API resources and webhooks, bulk import/export data, generate
-                checkout URLs, fulfill orders, and more without leaving the
-                command line.
-              </p>
-              <p className="mt-2 opacity-50 alg:mt-6 alg:text-lg alg:font-medium">Learn more {'->'}</p>
-            </div>
+          <div className="bg-[#272828] text-white p-6 rounded-lg">
+            <h2 className="text-xl font-medium alg:text-2xl">CLI</h2>
+            <p className="mt-2 opacity-50 alg:text-lg alg:mt-4">
+              Use our super-handy Command Line Interface to build a complete
+              ecommerce experience straight from the terminal. Manage all your
+              API resources and webhooks, bulk import/export data, generate
+              checkout URLs, fulfill orders, and more without leaving the
+              command line.
+            </p>
+            <p className="mt-2 opacity-50 alg:mt-6 alg:text-lg alg:font-medium">Learn more {'->'}</p>
           </div>
         </div>
       </div>
-    );
-  }
-  
-  export default DevExperience;
-  
+    </div>
+  );
+}
+
+export default DevExperience;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,12 +17,12 @@ function Navbar() {
     <nav className="mx-5 alg:mx-12 mt-4 xl:px-8">
       <section className="flex items-center">
         <figure className="flex items-center">
-          <img src={logo} alt="" className="w-16 h-16 alg:w-20 h-20" />
+          <img src={logo} alt="" className="w-16 alg:w-20 h-20" />
           <figcaption className="text-black text-xl font-bold font-manrope alg:text-[1.6rem]">ShopConnect</figcaption>
         </figure>
         <img className='w-9 rounded-lg h-9 ml-auto mr-4 xl:hidden' onClick={openNav} src={more} alt="" />
 
-        <aside className='hidden xl:block xl:flex xl:ml-auto cursor-pointer'>
+        <aside className='hidden xl:flex xl:ml-auto cursor-pointer'>
           <ul className='flex space-x-8 items-center font-medium text-base'>
             <li className='hover:text-[#666EFF]'>Why Commerce Layer</li>
             <li className='hover:text-[#666EFF]'>Customers</li>
@@ -31,14 +31,14 @@ function Navbar() {
             <li className='hover:text-[#666EFF]'>Blog</li>
           </ul>
           <div className='ml-10'>
-            <button className='border px-2 py-2 mx-4 rounded-lg font-medium text-[#666EFF] bg-blue-100 hover:bg-blue-50'>Sign in</button>
+            <button className='border px-3 py-2 mx-4 rounded-lg font-medium text-[#666EFF] bg-blue-100 hover:bg-blue-50'>Sign in</button>
             <button className='border px-2 py-2 rounded-lg font-medium bg-[#666EFF] text-white hover:bg-[#5458F0]'>Sign up for free</button>
           </div>
         </aside>
       </section>
 
       {/* nav sidebar below */}
-      <div className={`${nav ? 'md:fixed md:inset-0 md:z-40 md:bg-black md:backdrop-filter md:backdrop-blur-sm md:bg-opacity-40 md:w-full md:flex md:items-center md:justify-end xl:hidden md:overscroll-none': '' }`}>
+      <div className={`${nav ? 'md:fixed md:inset-0 md:z-40 md:bg-black md:backdrop-filter md:backdrop-blur-sm md:bg-opacity-40 md:w-full md:flex md:items-center md:justify-end xl:hidden md:overscroll-none' : ''}`}>
         <div
           className={`transform-gpu fixed inset-0 z-40 flex-none bg-white transition-transform ease-in-out duration-300 xl:hidden ${nav ? "translate-x-0 md:w-3/5 md:ml-auto md:mt-4 md:mb-4 md:rounded-lg md:mr-4" : "translate-x-full"
             }`}
